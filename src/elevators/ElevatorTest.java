6edd5b32aedb1032d1bbc45d7567aa51a5af55d2
Chris Simpauco <namenamenamenameme@gmail.com>
2021-11-30 19:49:08 -0500

    Worked on Elevators

--- src/elevators/ElevatorTest.java
+++ src/elevators/ElevatorTest.java
@@ -1,5 +1,6 @@
 package elevators;
 
 public class ElevatorTest {
-
+	Elevators eastElevator = new Elevators();
+	Elevators southElevator = new Elevators();
 }
--- src/elevators/Elevators.java
+++ src/elevators/Elevators.java
@@ -6,11 +6,53 @@ public class Elevators {
 	static int maxOccupants = 25;
 	final static boolean powerOn = true;
 
+	private int floor = 1;
+	private int people = 0;
+	private boolean doorsOpen = false;
+
 	Elevators() {
 
 	}
 
-	Elevators(int topFloor) {
+	Elevators(int floor) {
+		if (floor < 0 || floor > topFloor) {
+			floor = 0;
+		}
+	}
 
+	static void setPowerState () {
+	}
+
+	void up () {
+		floor++;
+	}
+
+	void down() {
+		floor--;
+	}
+
+	void goToFloor(boolean doorsOpen, boolean powerOn) {
+		if (doorsOpen = false && powerOn = true && floor+n <= topFloor) {
+			floor += n; 
+		} else System.out.println("Please check if doors are closed, the power is on and you only to to the top floor");
+	}
+
+	void addPeople() {
+		if (doorsOpen = true && people+n <= maxOccupants && n >= 0) {
+			people+= n; 
+		}
+	}
+
+	void removePeople() {
+		if (doorsOpen = true && people-n >= 0 && n >= 0) {
+			people-= n; 
+		}
+	}
+
+	@Override
+	public String toString() {
+		String s = " Floor=" + floor + ", people=" + people + ", doors are " + (doorsOpen ? "Open" : "Closed");
+		s = "Building power is " + (powerOn ? "ON" : "OFF") + s;
+		return s;
 	}
 }
